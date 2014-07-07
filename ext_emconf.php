15ba8c0c7e15a62d0ae0741a5cf20c36000590ef
glaman <manuel@manuelglauser.ch>
2014-07-07 10:49:09 +0200

    Updated dependencies / EM_CONF

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,43 +9,42 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Website templatebootstrap',
-	'description' => '',
-	'category' => 'fe',
-	'shy' => 0,
-	'dependencies' => 'extbase,fluid',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => 0,
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'author' => 'Stämpfli AG, Internet+Systeme Team 1',
-	'author_email' => 'info@staempfli.com',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'version' => '0.0.1',
-	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.1.0-0.0.0',
-			'typo3' => '6.1.0-0.0.0',
-			'fluid' => '',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-			'news' => '1.3.0-',
-		),
-	),
-	'suggests' => array(
-	),
+    'title' => 'Website templatebootstrap',
+    'description' => '',
+    'category' => 'fe',
+    'shy' => 0,
+    'dependencies' => 'extbase,fluid',  // DEPRECATED! Use 'constraints' instead!
+    'conflicts' => '',                  // DEPRECATED! Use 'constraints' instead!
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => 0,
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 1,
+    'lockType' => '',
+    'author' => 'Stämpfli AG, Internet+Systeme Team 1',
+    'author_email' => 'info@staempfli.com',
+    'author_company' => '',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'version' => '0.0.1',
+    '_md5_values_when_last_written' => '',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.1.0-0.0.0',
+            'typo3' => '6.1.0-0.0.0',
+            'fluid' => '',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(
+            'news' => '1.3.0-',
+            'realurl' => '1.12.1-',
+        ),
+    ),
+    'suggests' => array(),
 );
 
 ?>
