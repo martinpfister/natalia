f257cdf344ce7eb6976961bf5cf6fedbcb40e853
Manuel Glauser <manuel.glauser@staempfli.com>
2014-10-31 15:57:46 +0100

    Added "generateTrustedHostPatterns" to ext config.

--- Classes/Utility/PostInstallFileHandler.php
+++ Classes/Utility/PostInstallFileHandler.php
@@ -4,11 +4,15 @@ namespace Staempfli\TemplateBootstrap\Utility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-
 class PostInstallFileHandler {
 
+
+    public function getPackageKey() {
+        return $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['Staempfli/TemplateBootstrap']['PackageKey'];
+    }
+
     /**
-     * Creates or replaces robots.txt on save
+     * Creates or replaces robots.txt on save.
      *
      * @param $newConfiguration
      * @param $configurationController
@@ -16,7 +20,7 @@ class PostInstallFileHandler {
     public function handleRobotsTxt($newConfiguration, $configurationController) {
         global $BE_USER;
 
-        $packageKey                 = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['Staempfli/TemplateBootstrap']['PackageKey'];
+        $packageKey                 = $this->getPackageKey();
         $robotsTemplateStartMarker  = '# Inserted by ext. '. $packageKey .' - start';
         $robotsHint                 = '# (may be overwritten by ext manager as long as the enclosing comments persist)';
         $robotsTemplateEndMarker    = '# Inserted by ext. '. $packageKey .' - end';
@@ -84,6 +88,80 @@ class PostInstallFileHandler {
         return;
 
     } // function handleRobotsTxt
+
+
+
+
+    /**
+     * Renders and writes configuration that needs to go into AdditionalConfiguration.php
+     *
+     * @param $newConfiguration
+     * @param $configurationController
+     */
+    public function writeAdditionalConfiguration($newConfiguration, $configurationController) {
+        global $BE_USER;
+        $packageKey = $this->getPackageKey();
+        $currentConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$packageKey]);
+
+        // pageNotFound handler
+        // set default
+        if (!isset($currentConfiguration['enableCustomErrorHandling'])) {
+            $currentConfiguration['enableCustomErrorHandling'] = false;
+        }
+        // rewrite configuration, if necessary.
+        $enableCustomErrorHandler = boolval($newConfiguration['enableCustomErrorHandling']['value']);
+        if ($enableCustomErrorHandler == true && $enableCustomErrorHandler != $currentConfiguration['enableCustomErrorHandling']) {
+            $fileContentLines[] = '$GLOBALS[\'TYPO3_CONF_VARS\'][\'FE\'][\'pageNotFound_handling\'] = \'USER_FUNCTION:typo3conf/ext/' . $packageKey . '/Classes/Utility/PageNotFoundHandler.php:Staempfli\\Templatebootstrap\\Utility\\PageNotFoundHandler->pageNotFound\'';
+        }
+
+
+        // trustedHostsPattern
+        if (boolval($newConfiguration['generateTrustedHostsPattern']['value'])) {
+            $finalPattern = 'SERVER_NAME';
+            $domainsResult = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'sys_domain');
+            if ($GLOBALS['TYPO3_DB']->sql_num_rows($domainsResult)) {
+                $domainNames = Array();
+                while($domainRecord = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($domainsResult)) {
+                    $domainNames[] = str_replace('.', '\.', $domainRecord['domainName']);
+                }
+                $finalPattern = '^('. implode('|', $domainNames) .')$';
+            }
+            if ($GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] !== $finalPattern) {
+                $fileContentLines[] = '$GLOBALS[\'TYPO3_CONF_VARS\'][\'SYS\'][\'trustedHostsPattern\'] = \''. $finalPattern .'\'';
+            }
+        }
+
+
+
+        // Nothing to write?
+        // abort.
+        if (!count($fileContentLines)) {
+            $BE_USER->simplelog('(extension configuration) AdditionalConfiguration.php not written. No content for writing configured in ext configuration.', $packageKey, 0);
+            return;
+        }
+
+        // Enclose content with php tags & comment
+        array_unshift($fileContentLines, '# Automatically (re-)written by extension "'. $packageKey. '". ('. date('d.m.Y H:i:s') .')');
+        array_unshift($fileContentLines, '<?php');
+        $fileContentLines[] = ' ?>';
+
+        // Write file
+        $file = @fopen(PATH_typo3conf .'/AdditionalConfiguration.php', 'a');
+        $written = false;
+        if ($file) {
+            $written = @fwrite($file, implode(chr(10), $fileContentLines));
+            @fclose($file);
+        }
+
+        // Write log
+        if ($written) {
+            $BE_USER->simplelog('(extension configuration) AdditionalConfiguration.php written.', $packageKey, 0);
+        } else {
+            $BE_USER->writeLog('(extension configuration) Attempted to extend AdditionalConfiguration.php, but failed!', $packageKey, 2);
+        }
+
+    } // writeAdditionalConfiguration
+
 } // class PostInstallFileHandler
 
 ?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -6,9 +6,7 @@ if (!defined('TYPO3_MODE')) {
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-
 $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-
 
 # Adding page and user tsconfig
 # Parse files and replace ###PACKAGE_KEY### though!
@@ -23,7 +21,6 @@ $userTSConfigAbsFileName = GeneralUtility::getFileAbsFileName('EXT:' . $_EXTKEY 
 $userTSConfig = GeneralUtility::getUrl($userTSConfigAbsFileName);
 $userTSConfig = str_replace('###PACKAGE_KEY###', $_EXTKEY, $userTSConfig);
 ExtensionManagementUtility::addUserTSConfig($userTSConfig);
-
 
 # Register constants to use package key and version in TS later
 ExtensionManagementUtility::addTypoScriptConstants('plugin.templatebootstrap.packageKey='. $_EXTKEY);
@@ -41,18 +38,28 @@ if ($environment == 'local' || $environment == 'development' || $environment == 
 # Register extconf variable to use in scripts
 # (such as the layout provider hook)
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['Staempfli/TemplateBootstrap']['PackageKey'] = $_EXTKEY;
-$GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFound_handling'] = 'USER_FUNCTION:typo3conf/ext/'. $_EXTKEY .'/Classes/Utility/PageNotFoundHandler.php:Staempfli\\Templatebootstrap\\Utility\\PageNotFoundHandler->pageNotFound';
 
 # Use signal 'afterExtensionConfigurationWrite' to handle post installation tasks
 if (TYPO3_MODE === 'BE') {
     GeneralUtility::requireOnce(ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Utility/PostInstallFileHandler.php');
     $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
+
+    // Handle/write robots.txt
     $signalSlotDispatcher->connect(
         'TYPO3\\CMS\\Extensionmanager\\Controller\\ConfigurationController',
         'afterExtensionConfigurationWrite',
         'Staempfli\\TemplateBootstrap\\Utility\\PostInstallFileHandler',
         'handleRobotsTxt'
     );
+
+    // Handle/write AdditionalConfiguration.php
+    $signalSlotDispatcher->connect(
+        'TYPO3\\CMS\\Extensionmanager\\Controller\\ConfigurationController',
+        'afterExtensionConfigurationWrite',
+        'Staempfli\\TemplateBootstrap\\Utility\\PostInstallFileHandler',
+        'writeAdditionalConfiguration'
+    );
+
 }
 
 ?>
