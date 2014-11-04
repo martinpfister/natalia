df4ec40c27ada23566df04a25764de2250b89488
Manuel Glauser <manuel.glauser@staempfli.com>
2014-11-04 15:46:04 +0100

    Further implementation social media meta content. Included news detail view.

--- Resources/Private/TypoScript/Page/page_socialmediametatags.ts
+++ Resources/Private/TypoScript/Page/page_socialmediametatags.ts
@@ -1,19 +1,18 @@
+# **********************************************************
 # Prepare contents to reuse in multiple meta tags
+# **********************************************************
 temp.socialmediametacontent = COA
 temp.socialmediametacontent {
 
-    site_name = TEXT
-    site_name.value = {$company.name}
-
+    site_name = {$company.name}
     title = TEXT
     title.field = title
-
-    description = TEXT
     description.field = description
 
-    url = TEXT
     url.typolink {
         parameter.field = uid
+        addQueryString = 1
+        useCacheHash = 0
         returnLast = url
         forceAbsoluteUrl = 1
     }
@@ -22,7 +21,7 @@ temp.socialmediametacontent {
     image {
         cObject = FILES
         # Limit to first found image
-        cObject.stdWrap.listNum = 1
+        cObject.stdWrap.listNum = 0
         cObject {
             references.data = levelmedia:-1,slide
             renderObj = COA
@@ -39,28 +38,92 @@ temp.socialmediametacontent {
             }
         }
     }
+
+    # Social network specific values
+    openGraphType = website
+    twitterCardType = summary
 }
 
 
-# Render social media meta tags
+# **********************************************************
+# Override values for news detail view
+# **********************************************************
+[globalVar = GP:tx_news_pi1|news > 0]
+    temp.socialmediametacontent {
+
+        title >
+        title.cObject = COA
+        title.cObject {
+            10 = TEXT
+            10 {
+                data = GP:tx_news_pi1|news
+                wrap = {DB:tx_news_domain_model_news:|:title}
+                insertData = 1
+                if.isFalse.cObject = TEXT
+                if.isFalse.cObject {
+                    data = GP:tx_news_pi1|news
+                    wrap = {DB:tx_news_domain_model_news:|:alternative_title}
+                    insertData = 1
+                }
+            }
+            20 < .10.if.isFalse.cObject
+        }
+
+        description >
+        description.cObject = COA
+        description.cObject {
+            10 = TEXT
+            10 {
+                data = GP:tx_news_pi1|news
+                wrap = {DB:tx_news_domain_model_news:|:teaser}
+                insertData = 1
+                if.isFalse.cObject = TEXT
+                if.isFalse.cObject {
+                    data = GP:tx_news_pi1|news
+                    wrap = {DB:tx_news_domain_model_news:|:description}
+                    insertData = 1
+                }
+            }
+            20 < .10.if.isFalse.cObject
+        }
+
+
+        image.cObject.references {
+            data =
+            uid.data = GP:tx_news_pi1|news
+            table = tx_news_domain_model_news
+            fieldName = fal_media
+        }
+
+        # Social network specific values
+        openGraphType = article
+    }
+[global]
+
+
+
+# **********************************************************
+# Create tags - if this feature is enabled
+# **********************************************************
 [globalVar = LIT:1 = {$site.enableSocialMediaMetaTags}]
-page.meta {
-    # Facebook Open Graph
-    og:title < temp.socialmediametacontent.title
-    og:site_name < temp.socialmediametacontent.site_name
-    # Valid types are "article", "website" or "product"
-    og:type = website
-    og:locale < config.locale_all
-    og:description < temp.socialmediametacontent.description
-    og:url < temp.socialmediametacontent.url
-    og:image < temp.socialmediametacontent.image
+    page.meta {
+        # Facebook Open Graph
+        og:title < temp.socialmediametacontent.title
+        og:site_name < temp.socialmediametacontent.site_name
+        # Valid types are "article", "website" or "product"
+        og:type < temp.socialmediametacontent.openGraphType
+        og:locale < config.locale_all
+        og:description < temp.socialmediametacontent.description
+        og:url < temp.socialmediametacontent.url
+        og:image < temp.socialmediametacontent.image
 
-    # Twitter card
-    # Valid types are "summary", "summary_large_image", "photo", "gallery", "app", "player", "product"
-    # See https://dev.twitter.com/cards/types
-    twitter:card = summary
-    twitter:title < temp.socialmediametacontent.title
-    twitter:description < temp.socialmediametacontent.description
-    twitter:image < temp.socialmediametacontent.image
-}
+        # Twitter card
+        # Valid types are "summary", "summary_large_image", "photo", "gallery", "app", "player", "product"
+        # See https://dev.twitter.com/cards/types
+        twitter:card < temp.socialmediametacontent.twitterCardType
+        twitter:title < temp.socialmediametacontent.title
+        twitter:description < temp.socialmediametacontent.description
+        twitter:image < temp.socialmediametacontent.image
+        twitter:url < temp.socialmediametacontent.url
+    }
 [global]
